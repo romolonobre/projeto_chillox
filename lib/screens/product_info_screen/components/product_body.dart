8b88b85d376cd0b45ceba33f212d3fa9f1e1be57
Romolo Nobre <romoloferreira@icloud.com>
2021-09-12 15:58:30 +1000

    Page scrolling changed from SingleChildScrollView to Slivers (CustumScrollView)

--- lib/screens/product_info_screen/components/product_body.dart
+++ lib/screens/product_info_screen/components/product_body.dart
@@ -13,181 +13,185 @@ class ProductBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          ///
-          /// imager header
-          ///
-          Container(
-            height: 350,
-            width: double.infinity,
-            decoration: const BoxDecoration(
-              color: Color(0xff2f2f3d),
-            ),
-            child: Stack(
-              clipBehavior: Clip.none,
-              children: [
-                ///
-                ///image header
-                ///
-                Align(
-                  alignment: Alignment.center,
-                  child: DecoratedBox(
-                    decoration: const BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                            color: Colors.orange,
-                            blurRadius: 200,
-                            spreadRadius: -80,
-                            offset: Offset(0, 1)),
-                      ],
-                    ),
-                    child: Image.asset(
-                      popularFood.imageUrl,
-                      height: 300,
-                      width: 250,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              ///
+              /// imager header
+              ///
+              Container(
+                height: 350,
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Color(0xff2f2f3d),
                 ),
-
-                ///
-                ///appbar buttons
-                ///
-
-                Padding(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 35),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Buttom(
-                        icon: const Icon(
-                          Icons.arrow_back,
-                        ),
-                        onTap: () {},
-                        backgroundColor: Colors.white,
-                      ),
-                      Buttom(
-                        icon: const Icon(
-                          Icons.favorite_border_outlined,
-                        ),
-                        onTap: () {},
-                        backgroundColor: Colors.white,
-                      ),
-                    ],
-                  ),
-                ),
-                FoodQuantityButtons()
-              ],
-            ),
-          ),
-          const SizedBox(height: 70),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                child: Stack(
+                  clipBehavior: Clip.none,
                   children: [
-                    Heading(
-                      popularFood.name,
-                      fontWeight: FontWeight.w800,
-                    ),
-                    Text(
-                      popularFood.price,
-                      style: const TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
+                    ///
+                    ///image header
+                    ///
+                    Align(
+                      alignment: Alignment.center,
+                      child: DecoratedBox(
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                                color: Colors.orange,
+                                blurRadius: 200,
+                                spreadRadius: -80,
+                                offset: Offset(0, 1)),
+                          ],
+                        ),
+                        child: Image.asset(
+                          popularFood.imageUrl,
+                          height: 300,
+                          width: 250,
+                          fit: BoxFit.contain,
+                        ),
                       ),
                     ),
+
+                    ///
+                    ///appbar buttons
+                    ///
+
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 35),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Buttom(
+                            icon: const Icon(
+                              Icons.arrow_back,
+                            ),
+                            onTap: () {},
+                            backgroundColor: Colors.white,
+                          ),
+                          Buttom(
+                            icon: const Icon(
+                              Icons.favorite_border_outlined,
+                            ),
+                            onTap: () {},
+                            backgroundColor: Colors.white,
+                          ),
+                        ],
+                      ),
+                    ),
+                    FoodQuantityButtons()
                   ],
                 ),
-                const SizedBox(height: 5),
-                Heading(
-                  popularFood.subdescription,
-                  heading: Headings.h5,
-                  color: Colors.grey,
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              ),
+              const SizedBox(height: 70),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    DeliveryInfo(
-                        IconButton(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Heading(
+                          popularFood.name,
+                          fontWeight: FontWeight.w800,
+                        ),
+                        Text(
+                          popularFood.price,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 5),
+                    Heading(
+                      popularFood.subdescription,
+                      heading: Headings.h5,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(height: 15),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        DeliveryInfo(
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.moped_rounded,
+                                )),
+                            popularFood.delivery),
+                        DeliveryInfo(
+                            IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  Icons.moped_rounded,
+                                )),
+                            popularFood.deliverytime),
+                        DeliveryInfo(
+                          IconButton(
                             onPressed: () {},
-                            icon: const Icon(
-                              Icons.moped_rounded,
-                            )),
-                        popularFood.delivery),
-                    DeliveryInfo(
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.moped_rounded,
-                            )),
-                        popularFood.deliverytime),
-                    DeliveryInfo(
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.star_rate_rounded,
-                          color: Colors.yellow.shade800,
+                            icon: Icon(
+                              Icons.star_rate_rounded,
+                              color: Colors.yellow.shade800,
+                            ),
+                          ),
+                          popularFood.review,
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 15),
+                    Heading(
+                      'Ingredients',
+                      heading: Headings.h3,
+                      fontWeight: FontWeight.w800,
+                    ),
+                    const SizedBox(height: 10),
+                    const Placeholder(
+                      fallbackHeight: 100,
+                    ),
+                    const SizedBox(height: 15),
+                    Heading(
+                      'About',
+                      heading: Headings.h4,
+                      fontWeight: FontWeight.w800,
+                    ),
+                    const SizedBox(height: 10),
+                    Heading(
+                      popularFood.fullDescription,
+                      heading: Headings.h5,
+                      color: Colors.grey,
+                    ),
+                    const SizedBox(height: 10),
+                    Container(
+                      height: 80,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Color(0xffff663a),
+                        borderRadius: BorderRadius.circular(
+                          20,
                         ),
                       ),
-                      popularFood.review,
+                      child: Center(
+                        child: Text(
+                          'Add to cart ${popularFood.price}',
+                          style: const TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     )
                   ],
                 ),
-                const SizedBox(height: 15),
-                Heading(
-                  'Ingredients',
-                  heading: Headings.h3,
-                  fontWeight: FontWeight.w800,
-                ),
-                const SizedBox(height: 10),
-                const Placeholder(
-                  fallbackHeight: 100,
-                ),
-                const SizedBox(height: 15),
-                Heading(
-                  'About',
-                  heading: Headings.h4,
-                  fontWeight: FontWeight.w800,
-                ),
-                const SizedBox(height: 10),
-                Heading(
-                  popularFood.fullDescription,
-                  heading: Headings.h5,
-                  color: Colors.grey,
-                ),
-                const SizedBox(height: 10),
-                Container(
-                  height: 80,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    color: Color(0xffff663a),
-                    borderRadius: BorderRadius.circular(
-                      20,
-                    ),
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Add to cart ${popularFood.price}',
-                      style: const TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                )
-              ],
-            ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
